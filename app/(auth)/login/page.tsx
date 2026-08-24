493037b39ad07026b25904130e2aadb4c365c19f
iarms <systemiarms@gmail.com>
2026-08-24 14:18:31 +0800

    added 2 dots

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -106,7 +106,7 @@ function LoginContent() {
               <div className="space-y-1">
                 <h2 className="text-xl sm:text-[22px] font-black text-slate-900 tracking-tight">Portal Sign In</h2>
                 <p className="text-xs text-slate-600 font-medium">
-                  Access your association records &amp; financial statements.
+                  Access your association records &amp; financial statements..
                 </p>
               </div>
             </div>
